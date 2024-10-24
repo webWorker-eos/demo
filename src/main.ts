2fbdcd516437c368062deff72d9335afb7aa16ae
x <1574533602@qq.com>
2024-10-24 10:52:30 +0800

    feat: 公用样式引入

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 import pinia from '@/store';
-import '@/styles/reset.css';
+import '@/styles/index.less';
 
 createApp(App).use(pinia).use(router).mount('#app');
