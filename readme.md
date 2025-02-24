b82e372c53a8d0293db7c76ab9ccdf9f0ef56c47
webWorker <xxx>
2025-02-24 08:20:38 +0000

    Deploy to GitHub Pages ecbde131e270290b1a40e74c4b4fcb3d43869e1f

--- readme.md
+++
@@ -1,5 +0,0 @@
-# npm i
-
-# npm run dev
-
-# npm run build
--- src/router/modules/market.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  path: '/market',
-  name: 'market',
-  component: () => import('@/views/market/Index.vue'),
-  meta: {},
-  children: [],
-};
